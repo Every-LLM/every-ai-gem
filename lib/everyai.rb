9431cbb9e57133482994e528f784b726960865ae
Indigo Tech <yungboiiindigo@gmail.com>
2024-09-04 23:28:02 -0500

    Option to install model if its not already installed and ls function to print available models from the gem

--- lib/everyai.rb
+++ lib/everyai.rb
@@ -1,18 +1,36 @@
 require "ollama-ai"
 
 class Everyai
+  attr_reader :client
+  def initialize
+    @client = Ollama.new(
+      credentials: { address: 'http://localhost:11434' },
+      opiions: { server_sent_events: true }
+    )
+  end
+
+  def generate(prompt, model)
+    result = client.generate(
+      { model: model,
+        prompt: prompt }
+    )
+    result.map { |r| r['response']}.join
+  rescue Ollama::Errors::RequestError
+    puts "This Ollama model is not installed. Type y to install or any key to continue"
+    answer = gets.chomp.downcase
+    `ollama run #{model}` if answer == "y"
+  end
+
   class << self
     def generate(prompt, model: "llama3.1")
-      client = Ollama.new(
-        credentials: { address: 'http://localhost:11434' },
-        opiions: { server_sent_events: true }
-      )
+      @ai ||= new
+      @ai.generate(prompt, model)
+    end
 
-      result = client.generate(
-        { model: model,
-          prompt: prompt }
-      )
-      result.map { |r| r['response']}.join
+    def ls
+      puts "Listing Ollama AI Models"
+      str = `ollama list`
+      puts str.split("\n")
     end
   end
 end
